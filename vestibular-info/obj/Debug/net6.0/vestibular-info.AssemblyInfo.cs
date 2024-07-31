f57641b7730d246a982a3db759e431d67397cfd1
Weksslei Rodrigues <weksslei@easyteam.rocks>
2024-07-30 22:56:49 -0300

    missing files

--- vestibular-info/obj/Debug/net6.0/vestibular-info.AssemblyInfo.cs
+++ vestibular-info/obj/Debug/net6.0/vestibular-info.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vestibular-info")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d37637ed95171f29f8ea95618ca0d29f15935e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15d978631f771f89a265ab9a962042eb47db983b")]
 [assembly: System.Reflection.AssemblyProductAttribute("vestibular-info")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vestibular-info")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
