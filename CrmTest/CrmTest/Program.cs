dc2b4be6e05c4213b3a464a2e0416a24418f47e5
Patrick M. De Luca <patrick_mdl@outlook.com>
2024-08-10 21:20:09 -0300

    Projeto criado.

---
+++ CrmTest/CrmTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrmTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
