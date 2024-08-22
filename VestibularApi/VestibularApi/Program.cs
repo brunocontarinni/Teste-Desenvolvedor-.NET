9e2f62176df4c4bbb9e5f7060988201bc01254e9
Rafa Souza <31011616+RafaellSouzza@users.noreply.github.com>
2024-08-22 00:40:51 -0300

    Criação  do banco - ajuste do Program

--- VestibularApi/VestibularApi/Program.cs
+++ VestibularApi/VestibularApi/Program.cs
@@ -12,6 +12,14 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    dbContext.Database.EnsureCreated();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
