d340ef473a0c2a6d0a8fe1f3a68d5c496fb301f6
Tyrone Amorim <tyrone@trustdeveloper.com.br>
2025-01-30 11:44:15 -0300

    Migrations files added

---
+++ src/Infrastructure/Migrations/20250131011224_InitialCreate.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidatos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Telefone = table.Column<string>(type: "text", nullable: true),
+                    Cpf = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidatos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ofertas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    Descricao = table.Column<string>(type: "text", nullable: true),
+                    VagasDisponiveis = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ofertas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProcessosSeletivos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    DataInicio = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    DataTermino = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcessosSeletivos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inscricoes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    NumeroInscricao = table.Column<int>(type: "integer", nullable: false),
+                    Data = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Status = table.Column<char>(type: "character(1)", nullable: false),
+                    CandidatoId = table.Column<int>(type: "integer", nullable: true),
+                    ProcessoSeletivoId = table.Column<int>(type: "integer", nullable: true),
+                    OfertaId = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inscricoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Inscricoes_Candidatos_CandidatoId",
+                        column: x => x.CandidatoId,
+                        principalTable: "Candidatos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Inscricoes_Ofertas_OfertaId",
+                        column: x => x.OfertaId,
+                        principalTable: "Ofertas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Inscricoes_ProcessosSeletivos_ProcessoSeletivoId",
+                        column: x => x.ProcessoSeletivoId,
+                        principalTable: "ProcessosSeletivos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inscricoes_CandidatoId",
+                table: "Inscricoes",
+                column: "CandidatoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inscricoes_OfertaId",
+                table: "Inscricoes",
+                column: "OfertaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Inscricoes_ProcessoSeletivoId",
+                table: "Inscricoes",
+                column: "ProcessoSeletivoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Inscricoes");
+
+            migrationBuilder.DropTable(
+                name: "Candidatos");
+
+            migrationBuilder.DropTable(
+                name: "Ofertas");
+
+            migrationBuilder.DropTable(
+                name: "ProcessosSeletivos");
+        }
+    }
+}
